38f40850e2fbe37e706b4bfc9ceb2f8881a21361
Saif Yahyaoui <saif.yahyaoui@esprit.tn>
2024-02-22 18:54:11 +0100

    Initial Comment

---
+++ fals/lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:fals/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:fals/utils/theme/theme.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+        themeMode: ThemeMode.system,
+        theme: TAppTheme.lightTheme,
+        darkTheme: TAppTheme.darkTheme,
+        debugShowCheckedModeBanner: false,
+        home: const OnBoardingScreen());
+  }
+}
