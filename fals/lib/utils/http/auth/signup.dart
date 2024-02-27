d35e830a8f14ce3f3d2ecd4ac0292c21382af77d
Saif Yahyaoui <saif.yahyaoui@esprit.tn>
2024-02-27 16:00:05 +0100

    Signup Backend Logic

--- fals/lib/utils/http/auth/signup.dart
+++ fals/lib/utils/http/auth/signup.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+
 Future<void> signupUser(
   String username,
   String fistName,
@@ -9,7 +10,7 @@ Future<void> signupUser(
   String phoneNumber,
   String password,
 ) async {
-  final url = Uri.parse('http://localhost:9090/auth//signup');
+  final url = Uri.parse('http://localhost:9090/auth/signup');
   final response = await http.post(
     url,
     body: json.encode({
