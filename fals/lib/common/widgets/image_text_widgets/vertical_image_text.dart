18602c8d2b967ed4028ff9ee9bb4004303b594bc
Saif Yahyaoui <saif.yahyaoui@esprit.tn>
2024-02-27 15:57:34 +0100

    Horizontal Image Slider

---
+++ fals/lib/common/widgets/image_text_widgets/vertical_image_text.dart
@@ -0,0 +1,72 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/sizes.dart';
+import '../../../utils/helpers/helper_functions.dart';
+
+class VerticalImageText extends StatelessWidget {
+  const VerticalImageText({
+    super.key,
+    required this.image,
+    this.textColor = TColors.white,
+    this.backgroundColor,
+    this.onTap,
+    required this.title,
+  });
+
+  final String image, title;
+  final Color textColor;
+  final Color? backgroundColor;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.only(right: TSizes.spaceBtwItems),
+        child: Column(
+          children: [
+            /// Circular Icon
+            Container(
+              width: 65,
+              height: 65,
+              padding: EdgeInsets.all(TSizes.sm),
+              decoration: BoxDecoration(
+                color: backgroundColor ?? (dark ? TColors.black : TColors.white ),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Center(
+                child: Image(
+                  image: AssetImage(image),
+                  fit: BoxFit.cover,
+                  color: dark ? TColors.light : TColors.dark ,
+                ),
+              ),
+            ),
+
+            /// Text
+            const SizedBox(
+              height: TSizes.spaceBtwItems / 2,
+            ),
+            SizedBox(
+              width: 65,
+              child: Text(
+                title,
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .apply(color: textColor),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
