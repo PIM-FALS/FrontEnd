ce897248dcf85965472313529f242cf74738b0e7
Saif Yahyaoui <saif.yahyaoui@esprit.tn>
2024-02-27 15:57:19 +0100

    Search section

--- fals/lib/common/widgets/custom_shapes/containers/search_container.dart
+++ fals/lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -9,30 +9,47 @@ import '../../../../utils/helpers/helper_functions.dart';
 
 class SearchContainer extends StatelessWidget {
   const SearchContainer({
-    super.key, required this.text, this.icon = Iconsax.search_normal, this.showBackground = true, this.showBorder = true,
+    super.key,
+    required this.text,
+    this.icon = Iconsax.search_normal,
+    this.showBackground = true,
+    this.showBorder = true, this.onTap,
   });
-  final String text ;
+
+  final String text;
+
   final IconData? icon;
-  final bool showBackground,showBorder;
+  final bool showBackground, showBorder;
+  final void Function()? onTap;
+
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
-      child: Container(
-        width: TDeviceUtils.getScreenWidth(context) ,
-        padding: const EdgeInsets.all(TSizes.md),
-        decoration: BoxDecoration(
-          color: showBackground ? TColors.light :  Colors.transparent,
-          borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
-          border:showBorder ? Border.all(color: TColors.darkerGrey.withOpacity(0.3)) : null,
-        ),
-        child: Row(
-          children: [
-            Icon(icon, color: TColors.darkerGrey),
-            const SizedBox(width: TSizes.spaceBtwItems),
-            Text(text, style: Theme.of(context).textTheme.bodySmall!.apply(color: TColors.darkGrey)),
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+        child: Container(
+          width: TDeviceUtils.getScreenWidth(context),
+          padding: const EdgeInsets.all(TSizes.md),
+          decoration: BoxDecoration(
+            color: showBackground ? TColors.light : Colors.transparent,
+            borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
+            border: showBorder
+                ? Border.all(color: TColors.darkerGrey.withOpacity(0.3))
+                : null,
+          ),
+          child: Row(
+            children: [
+              Icon(icon, color: TColors.darkerGrey),
+              const SizedBox(width: TSizes.spaceBtwItems),
+              Text(text,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodySmall!
+                      .apply(color: TColors.darkGrey)),
+            ],
+          ),
         ),
       ),
     );
---
+++ fals/lib/common/widgets/texts/section_heading.dart
@@ -0,0 +1,45 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SectionHeading extends StatelessWidget {
+  const SectionHeading({
+    super.key,
+    this.textColor,
+    this.showActionButton = true,
+    required this.title,
+    this.buttonTitle = 'View All',
+    this.onPressed,
+  });
+
+  final Color? textColor;
+  final bool showActionButton;
+  final String title, buttonTitle;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style:
+          Theme.of(context).textTheme.titleLarge!.apply(color: textColor),
+        ),
+        if (showActionButton)
+          TextButton(
+            onPressed: onPressed,
+            child: Text(
+              buttonTitle,
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall!
+                  .apply(color: textColor),
+            ),
+          ),
+      ],
+    );
+  }
+}
